cb0de138ab7013e5e80872d67c1c00d780d6355c
schadley <schadley@umich.edu>
2019-07-22 20:30:03 -0400

    Small formatting fixes

--- PS1.sh
+++ PS1.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 BoxCornerUL='┌' #u250c
 BoxCornerLL='└' #u2514
 Tpipe='├' #u251c
@@ -10,9 +11,9 @@ Green='\e[0;32m'
 NC='\e[0m'
 
 if [ "$color_prompt" = yes ]; then
-    PS1='$BoxCornerUL$LineHoriz ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='$BoxCornerUL$LineHoriz ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]'
 else
-    PS1='$BoxCornerUL$LineHoriz ${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1='$BoxCornerUL$LineHoriz ${debian_chroot:+($debian_chroot)}\u@\h:\w'
 fi
 unset color_prompt force_color_prompt
 
